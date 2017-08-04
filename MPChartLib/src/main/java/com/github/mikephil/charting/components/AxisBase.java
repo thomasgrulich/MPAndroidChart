dc6a21d2ce1eee16dc9a3f4ba11dfc4bd6ef5050
Thomas Grulich <mail@grulich-software.com>
2017-08-04 17:33:38 +0200

    drawTickLines = false as default

--- MPChartLib/src/main/java/com/github/mikephil/charting/components/AxisBase.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/components/AxisBase.java
@@ -93,7 +93,7 @@ public abstract class AxisBase extends ComponentBase {
     /**
      * flag indicating if the tick lines for this axis should be drawn
      */
-    protected boolean mDrawTickLines = true;
+    protected boolean mDrawTickLines = false;
 
     /**
      * flag that indicates of the labels of this axis should be drawn or not
